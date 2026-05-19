b3a038909c45ba1c4160052003e18d9141345996
unknown <zhou-h@live.cn>
2026-05-19 21:47:52 +0800

    Update

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -166,6 +166,7 @@ sed -i '252{/else/s/^/#/}' lib/functions/cli/utils-cli.sh
 sed -i '253{/display_alert/s/^/#/}' lib/functions/cli/utils-cli.sh
 sed -i '272{/display_alert/s/^/#/}' lib/functions/cli/utils-cli.sh
 sed -i '383{/display_alert/s/^/#/}' lib/functions/main/config-prepare.sh
+sed -i '/display_alert "ORAS manifest fetch error"/c\		:' lib/functions/general/oci-oras.sh
 [ ! -d userpatches/extensions ] && mkdir -p userpatches/extensions
 
 # Set custom version automatically based on date
