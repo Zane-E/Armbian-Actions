890549d821bfb1f1a416c14aa5a9a91f067166e0
unknown <zhou-h@live.cn>
2026-09-04 13:10:30 +0800

    Update

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -116,9 +116,6 @@ echo "Copying 5T patches..."
 cp -f ${GITHUB_WORKSPACE}/patch/5T/* patch/kernel/rk35xx-vendor-6.1/
 sed -i 's|https://github.com/armbian/linux-rockchip.git|https://github.com/zane-e/linux-rockchip.git|g' config/sources/families/rk35xx.conf
 sed -i 's|https://github.com/armbian/linux-rockchip.git|https://github.com/zane-e/linux-rockchip.git|g' config/sources/families/rockchip-rk3588.conf
-sed -i 's/rk-6\.1-rkr5\.1/rk-6.1-rkr7.2/g; s/v6\.1\.115/v6.1.172/g' patch/kernel/rk35xx-vendor-6.1/0000.patching_config.yaml
-sed -i 's/branch:rk-6\.1-rkr5\.1/branch:rk-6.1-rkr7.2/g' config/sources/families/rockchip-rk3588.conf
-sed -i 's/branch:rk-6\.1-rkr5\.1/branch:rk-6.1-rkr7.2/g' config/sources/families/rk35xx.conf
 
 # N1 Patches
 echo "Copying N1 patches..."
@@ -146,9 +143,9 @@ cp -f ${GITHUB_WORKSPACE}/patch/TVI/rk3399-tvi3315a.dts patch/kernel/archive/roc
 # Radxa-cubie-a5e Patches for 1G ram
 cp -f ${GITHUB_WORKSPACE}/patch/A5E/edit-dram-parameters.patch patch/u-boot/v2026.01/board_radxa-cubie-a5e/
 cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/sunxi-6.18/patches.armbian/
-cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/sunxi-7.1/patches.armbian/
-sed -i '471a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-6.18/series.conf
-sed -i '429a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-7.1/series.conf
+cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/sunxi-7.2/patches.armbian/
+sed -i '475a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-6.18/series.conf
+sed -i '585a patches.armbian/fix-CPU-information-6.16.patch' patch/kernel/archive/sunxi-7.2/series.conf
 
 # Uefi-arm64 Patches
 cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/uefi-arm64-6.18/
